1246ff9fd16432b28db55de4c934bdb4bf5fb739
MininMobile <miningamer@gmail.com>
2017-12-23 19:24:30 +0000

    Add interpreter

--- data/index.js
+++ data/index.js
@@ -1,3 +1,33 @@
-const fs = require("fs");
+const fs = require("fs"),
+      util = require("util");
 
-console.log(fs.readFileSync("input.txt").toString());
+const readFile = util.promisify(fs.readFile);
+
+async function getInput(file) {
+  return await readFile(file);
+}
+// Simplify Input
+getInput("input.txt").then((data) => {
+    // Convert data to string
+    input = data.toString();
+
+    // p u r i f y
+    input = input.replace(" ", "");
+    input = input.replace(/\r?\n|\r/g, "");
+    input = input.replace(/[a-z]/g, "")
+    input = input.replace(/[0-9]/g, "")
+    
+    // Create Point Registry
+    var pointers = [0];
+    var pointer = -1;
+    
+    // Begin
+    var output = "";
+    for (let i = 0; i < input.length; i++) {
+        if (pointer < 0) throw RangeError(`Pointer accounts to under 0\nCharacter: ${i+1}`).stack;
+    }
+    
+    // Read Output
+    output += input;
+    console.log(input);
+}).catch(console.error);
