d709a75a257ab16a7c3d7932e59405843044ef30
MininMobile <notminin@gmail.com>
2018-06-17 07:37:21 +0100

    ELSE IF

--- index.js
+++ index.js
@@ -44,26 +44,26 @@ getInput("input.txt").then((data) => {
 		let c = input[i];
 
 		//#region Command Parser
-		if (c == "+") { pointers[pointer] += 1; }
-		if (c == "-") { pointers[pointer] -= 1; }
-		if (c == ">") { pointer++; if (pointer == pointers.length) { pointers.push(0); } }
-		if (c == "<") { pointer--; }
-		if (c == "!") { console.log(getChar(pointers[pointer])); }
-		if (c == "?") { console.log(pointer.toString()); }
-		if (c == "[") { jumpPoint.push(i); }
-		if (c == "]") { if (pointers[pointer] != 0) { i = jumpPoint[jumpPoint.length - 1] - 1; jumpPoint.pop(); } }
-		if (c == "{") { jumpPoint.push(i); }
-		if (c == "}") { if (pointers[pointer] == 0 || pointers[pointer] > 25) { i = jumpPoint[jumpPoint.length - 1] - 1; jumpPoint.pop(); } }
-		if (c == "(") { jumpPoint.push(i); }
-		if (c == ")") { if (pointers[pointer] < 26) { i = jumpPoint[jumpPoint.length - 1] - 1; jumpPoint.pop(); } }
-		if (c == "^") { pointers[pointer] = toPoint(args[pointer]); }
-		if (c == "*") { break }
+			 if (c == "+") { pointers[pointer] += 1; }
+		else if (c == "-") { pointers[pointer] -= 1; }
+		else if (c == ">") { pointer++; if (pointer == pointers.length) { pointers.push(0); } }
+		else if (c == "<") { pointer--; }
+		else if (c == "!") { console.log(getChar(pointers[pointer])); }
+		else if (c == "?") { console.log(pointer.toString()); }
+		else if (c == "[") { jumpPoint.push(i); }
+		else if (c == "]") { if (pointers[pointer] != 0) { i = jumpPoint[jumpPoint.length - 1] - 1; jumpPoint.pop(); } }
+		else if (c == "{") { jumpPoint.push(i); }
+		else if (c == "}") { if (pointers[pointer] == 0 || pointers[pointer] > 25) { i = jumpPoint[jumpPoint.length - 1] - 1; jumpPoint.pop(); } }
+		else if (c == "(") { jumpPoint.push(i); }
+		else if (c == ")") { if (pointers[pointer] < 26) { i = jumpPoint[jumpPoint.length - 1] - 1; jumpPoint.pop(); } }
+		else if (c == "^") { pointers[pointer] = toPoint(args[pointer]); }
+		else if (c == "*") { break }
 		//#endregion
 	}
 }).catch((message) => {
 	// When the an error, who you gonna call?
 	// ERROR BUSTERS!
-	console.error(message)
+	console.error(message);
 });
 
 // Convert Point to Char
@@ -76,6 +76,8 @@ function getChar(p) {
 // Convert Char to Point
 function toPoint(p) {
 	if (p == " ") return 0;
-	else if (/[a-z]/.test(p)) return "qwertyuiopasdfghjklzxcvbnm".indexOf(p);
-	else if (/[0-9]/.test(p)) return parseInt(p)+26;
+	else if (p == null) return 0;
+	else if (p == undefined) return 0;
+	else if (/[a-z]/.test(p)) return "qwertyuiopasdfghjklzxcvbnm".indexOf(p) + 1;
+	else if (/[0-9]/.test(p)) return parseInt(p)+27;
 }
