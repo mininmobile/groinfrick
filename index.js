e743966c53472fe808c2c5cd0ed3e656e9d1fbe3
not ervin <notminin@pm.me>
2020-03-17 01:47:23 +0000

    add get next input char

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ readFile(getInputFile()).then((data) => {
 	// define memory
 	let memory = [0];
 	let pointer = 0;
-	// define last char received from io
-	let inputindex = 0;
+	// pointer for input
+	let ipointer = 0;
 	// define all the loop backs and stuff
 	let loopback = [];
 	// no new line, to format ?, !, and . to play nicely with eachother
@@ -41,6 +41,7 @@ readFile(getInputFile()).then((data) => {
 			case "<": {
 				pointer--;
 
+				// pointer index will never be <0
 				if (pointer < 0)
 					pointer = 0;
 			} break;
@@ -49,6 +50,7 @@ readFile(getInputFile()).then((data) => {
 			case ">": {
 				pointer++;
 
+				// allocate new memory slot
 				if (memory[pointer] == undefined) {
 					memory[pointer] = [];
 				}
@@ -59,27 +61,47 @@ readFile(getInputFile()).then((data) => {
 			// decrement pointer
 			case "-": memory[pointer]++; break;
 
+			// get next input char
+			case ",": {
+				// check if input still has chars
+				if (args[ipointer]) {
+					// write ascii char to pointer
+					memory[pointer] = args[ipointer].charCodeAt();
+					// increment input pointer
+					ipointer++
+				} else {
+					// set pointer to zero
+					memory[pointer] = 0;
+				}
+			} break;
+
 			// echo pointer data (ascii)
 			case ".": {
 				if (memory[pointer] >= 0 && memory[pointer] < 128) {
+					// if in ascii bounds, write ascii char
 					process.stdout.write(String.fromCharCode(memory[pointer]));
 				} else {
+					// if not in ascii bounds, write number
 					process.stdout.write(memory[pointer]);
 				}
 
+				// we use process.stdout.write bc it doesn't write a new line,
+				// so we have to remember that for ? and ! which do
 				nnl = true;
 			} break;
 
 			// echo pointer data (hex)
 			case "!": {
+				// safety new line
 				if (nnl)
 					process.stdout.write("\n");
-
 				nnl = false;
+
+				// write pointer data as hexadecimal
 				console.log(`0x${memory[pointer].toString(16)}`);
 			} break;
 
-			// exit
+			// force exit
 			case "*": i = input.length; break;
 		}
 	}
